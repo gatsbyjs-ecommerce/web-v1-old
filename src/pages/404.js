7d5c070363eaabbebbe0cb3451e0cb6b72e47abb
Parminder Klair <perminder.klair@gmail.com>
2018-06-24 13:18:49 +0530

    show intersting products on 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,50 @@ import React from 'react';
 import Helmet from 'react-helmet';
 
 import config from '../config';
+import Heading from '../components/Heading';
+import ProductsList from '../components/ProductsList';
 
-const NotFoundPage = () => (
-  <div>
-    <Helmet title={`Not found - ${config.siteName}`} />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
-);
+const NotFoundPage = ({ data }) => {
+  const { allContentfulProduct: products } = data;
+
+  return (
+    <div>
+      <Helmet title={`Not found - ${config.siteName}`} />
+      <Heading>NOT FOUND</Heading>
+      <p className="has-text-centered is-size-5">
+        You just hit a route that doesn&#39;t exist... the sadness.
+      </p>
+      <br />
+      <ProductsList title="We think you'll" products={products.edges} />
+    </div>
+  );
+};
 
 export default NotFoundPage;
+
+export const notFoundQuery = graphql`
+  query notFoundQuery {
+    allContentfulProduct(
+      filter: { status: { eq: "active" } }
+      limit: 6
+      sort: { fields: [createdAt], order: DESC }
+    ) {
+      edges {
+        node {
+          id
+          title
+          slug
+          color
+          originalPrice
+          discountPrice
+          featuredImage {
+            title
+            sizes(maxWidth: 550) {
+              ...GatsbyContentfulSizes
+            }
+          }
+        }
+      }
+    }
+  }
+`;
