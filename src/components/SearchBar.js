5d1982cdf6a7e02eeb6dc35aa67d1bec42d71d8c
gagansaini1212 <gagansaini1212@gmail.com>
2019-07-21 14:25:17 +0530

    added filter

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -15,17 +15,38 @@ class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchFilter: [],
+      filter: '',
+      data: [
+        // {
+        //   brand: 'Samsung',
+        //   lname: 'Washington',
+        //   email: 'jaynewashington@exposa.com',
+        //   gender: 'female',
+        // },
+        // {
+        //   brand: 'LG',
+        //   lname: 'Dalton',
+        //   email: 'petersondalton@exposa.com',
+        //   gender: 'male',
+        // },
+      ],
     };
   }
-
   // TODO: Fix this with filter. Take reference from lodash
 
-  handleChange = () => {
-    this.setState();
+  handleChange = event => {
+    this.setState({ filter: event.target.value });
   };
 
   render() {
+    const { filter, data } = this.state;
+    const lowercasedFilter = filter.toLowerCase();
+    const filteredData = data.filter(item => {
+      return Object.keys(item).some(key =>
+        item[key].toLowerCase().includes(lowercasedFilter),
+      );
+    });
+
     return (
       <Container className="field search-bar">
         <p className="control has-icons-right">
@@ -33,10 +54,19 @@ class SearchBar extends React.Component {
             className="input is-radiusless"
             type="text"
             placeholder="Search"
+            value={filter}
+            onChange={this.handleChange}
           />
           <span className="icon is-small is-right">
             <i className="fas fa-search" />
           </span>
+          {filteredData.map(item => (
+            <div key={item.email}>
+              <div>
+                {item.fname} {item.lname} - {item.gender} - {item.email}
+              </div>
+            </div>
+          ))}
         </p>
       </Container>
     );
