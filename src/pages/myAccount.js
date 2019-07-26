00019fb77331cd85c2388333cc9806dd1a9aea5d
gagansaini1212 <gagansaini1212@gmail.com>
2019-07-26 14:06:09 +0530

    adeed my account mutation added

--- src/pages/myAccount.js
+++ src/pages/myAccount.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import swal from 'sweetalert';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
@@ -13,24 +16,61 @@ const Container = styled.div`
   }
 `;
 
-const MyAccount = () => (
-  <Layout>
-    <Container className="section">
-      <div className="container">
-        <Seo
-          title="My Account"
-          description="Upgate your account"
-          url={`${config.siteUrl}/myAccount`}
-        />
-        <Heading>My Account</Heading>
-        <div className="columns">
-          <div className="column is-half">
-            <MyAccountForm />
+const myAccountMutation = gql`
+  mutation updateMe($email: String!) {
+    updateMe(input: { email: $email }) {
+      id
+      email
+      status
+    }
+  }
+`;
+
+class MyAccount extends React.Component {
+  onSuccess = () => {
+    swal('Success!', 'Your Account has been successfully updated!');
+  };
+
+  render() {
+    return (
+      <Layout>
+        <Container className="section">
+          <div className="container">
+            <Seo
+              title="My Account"
+              description="Upgate your account"
+              url={`${config.siteUrl}/myAccount`}
+            />
+            <Heading>My Account</Heading>
+            <div className="columns">
+              <div className="column is-half">
+                <Mutation
+                  mutation={myAccountMutation}
+                  update={this.onSuccess}
+                  onError={error => {
+                    swal(
+                      'Issue!',
+                      error.message.replace('GraphQL error: ', ''),
+                      'warning',
+                    );
+                  }}>
+                  {myAccount => (
+                    <MyAccountForm
+                      handleUpdate={data => {
+                        return myAccount({
+                          variables: data,
+                        });
+                      }}
+                    />
+                  )}
+                </Mutation>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </Container>
-  </Layout>
-);
+        </Container>
+      </Layout>
+    );
+  }
+}
 
 export default MyAccount;
